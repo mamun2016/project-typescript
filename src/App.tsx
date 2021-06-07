0e7fb9fc95568007e48f6f16e24aad41785dd9fd
Mamun <kh.mamun@gmail.com>
2021-06-07 17:18:48 +0600

    Installed react, typescript and sass

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="container">
+      Learn Typescript with React JS
+    </div>
+  );
+}
+
+export default App;
